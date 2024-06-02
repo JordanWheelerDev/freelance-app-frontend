0e3b7b530660de1903d3e3238e159cee1c31fe86
JordanWheelerDev <163913618+JordanWheelerDev@users.noreply.github.com>
2024-06-01 22:41:47 -0230

    Minor update
    - added time formatting on the single view job page.
    - added a placeholder for the bid cost to send a proposal.

--- src/components/jobs/JobDetails.js
+++ src/components/jobs/JobDetails.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ProposalForm from './ProposalForm';
 import { Col, Row } from 'react-bootstrap';
 import DOMPurify from 'dompurify';
+import { formatDistanceToNow, parseISO, format } from 'date-fns';
 
 const JobDetails = ({ job }) => {
+  const formatPostedTime = (dateString) => {
+    const date = parseISO(dateString);
+    const now = new Date();
+    const timeDifference = now - date;
+
+    if (timeDifference < 24 * 60 * 60 * 1000) {
+      // If the date is within the last 24 hours, show relative time
+      return `${formatDistanceToNow(date, { addSuffix: true })}`;
+    } else {
+      // Otherwise, show formatted date
+      return format(date, 'MMMM d, yyyy');
+    }
+  };
+
   const sanitizedDescription = DOMPurify.sanitize(job.description);
   return (
     <>
@@ -14,7 +29,9 @@ const JobDetails = ({ job }) => {
             <div className="mb-4 job-section-subheading">Job Details</div>
             <div className="job-page-title">{job.title}</div>
             <div className="d-flex justify-content-between mb-3">
-              <div className="job-list-date">Posted: 1hr ago</div>
+              <div className="job-list-date">
+                Posted {formatPostedTime(job.posted_on)}
+              </div>
               <div className="job-list-report">Report</div>
             </div>
             <div className="mb-3">
@@ -60,7 +77,10 @@ const JobDetails = ({ job }) => {
       <div className="job-section">
         <Row>
           <Col>
-            <div className="mb-4 job-section-subheading">Proposal form</div>
+            <div className="mb-2 job-section-subheading">Proposal form</div>
+            <div className="mb-4">
+              This proposal will cost you x bids. You have a total of x bids.
+            </div>
             <ProposalForm jobId={job.id} />
           </Col>
         </Row>
