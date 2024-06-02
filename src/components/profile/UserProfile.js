107e34410b3366e5fb982822d323fb355e63889a
JordanWheelerDev <163913618+JordanWheelerDev@users.noreply.github.com>
2024-06-02 19:22:20 -0230

    Minor update
    - completed the profile page with the user about section.
    - profile page now has: users name, skills, headline, about, starting hourly rate, invite to job, and message.

--- src/components/profile/UserProfile.js
+++ src/components/profile/UserProfile.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { faMapPin } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import DOMPurify from 'dompurify';
 
 const UserProfile = ({ profileData }) => {
   const user = profileData.user; // Assuming profileData contains user and contracts
   const contracts = profileData.contracts;
+  const sanitizeUserAbout = DOMPurify.sanitize(user.about);
 
   // Calculate total amount earned and number of completed contracts
   const completedContracts = contracts.filter(
@@ -27,10 +29,21 @@ const UserProfile = ({ profileData }) => {
             <img src="https://placehold.co/400" alt={user.full_name} />
           </div>
           <div className="profile-user-name text-center">{user.full_name}</div>
-          <div className="profile-user-location">
+          <div className="profile-user-location text-center mb-3">
             <FontAwesomeIcon icon={faMapPin} /> {user.country}
           </div>
-          <div className="profile-user-name">{user.full_name}</div>
+          <div className="mb-2">
+            <button className="fl-button-primary" style={{ width: '100%' }}>
+              {' '}
+              Invite to Project
+            </button>
+          </div>
+          <div>
+            <button className="fl-button-primary" style={{ width: '100%' }}>
+              {' '}
+              Message
+            </button>
+          </div>
         </div>
         <div className="profile-earnings-sidebar">
           <div className="d-flex justify-content-between earning-details mb-3">
@@ -66,7 +79,19 @@ const UserProfile = ({ profileData }) => {
         ))} */}
       </Col>
       <Col md={9}>
-        <div className="profile-about-user"></div>
+        <div className="profile-about-user">
+          <div className="profile-headline mb-3">
+            <div className="d-flex justify-content-between mb-3">
+              <div>{user.headline}</div>
+              <div>Starting rate of ${user.hourly_rate}/hr</div>
+            </div>
+          </div>
+          <hr className="fancy-hr" />
+          <div
+            className="profile-user-description"
+            dangerouslySetInnerHTML={{ __html: sanitizeUserAbout }}
+          ></div>
+        </div>
       </Col>
     </Row>
   );
