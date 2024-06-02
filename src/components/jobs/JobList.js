670406c36429b1e4d7c4716431533d69fe802d8e
JordanWheelerDev <163913618+JordanWheelerDev@users.noreply.github.com>
2024-06-01 22:11:10 -0230

    Update JobList.js
    - made sure to hide pagination and show a message if 0 jobs in the database.

--- src/components/jobs/JobList.js
+++ src/components/jobs/JobList.js
@@ -37,82 +37,91 @@ const JobList = () => {
 
   return (
     <>
-      {jobs.map((job) => (
-        <Row key={job.id}>
-          <Col>
-            <div className="job-list-container">
-              <div className="d-flex justify-content-between">
-                <div className="job-list-date">Posted</div>
-                <div className="job-list-report">Report</div>
-              </div>
-              <div className="mb-2">
-                <Link
-                  to={`/job/${job.id}`}
-                  className="job-list-title no-decoration"
-                >
-                  {job.title}
-                </Link>
-              </div>
-              <div className="mb-2 d-flex justify-content-between">
-                <div className="job-pay-details">
-                  {job.pay_type === 'Per Project' && (
-                    <span>
-                      Per Project: ${new Intl.NumberFormat().format(job.budget)}
-                      .00 budget
-                    </span>
-                  )}
-                  {job.pay_type === 'Hourly' && (
-                    <span>
-                      Hourly: from $
-                      {new Intl.NumberFormat().format(job.min_pay)}/hr to $
-                      {new Intl.NumberFormat().format(job.max_pay)}/hr.
-                    </span>
-                  )}
+      {jobs.length === 0 ? (
+        <div className="text-center">
+          No jobs to display right now. Check back soon.
+        </div>
+      ) : (
+        jobs.map((job) => (
+          <Row key={job.id}>
+            <Col>
+              <div className="job-list-container">
+                <div className="d-flex justify-content-between">
+                  <div className="job-list-date">Posted</div>
+                  <div className="job-list-report">Report</div>
+                </div>
+                <div className="mb-2">
+                  <Link
+                    to={`/job/${job.id}`}
+                    className="job-list-title no-decoration"
+                  >
+                    {job.title}
+                  </Link>
+                </div>
+                <div className="mb-2 d-flex justify-content-between">
+                  <div className="job-pay-details">
+                    {job.pay_type === 'Per Project' && (
+                      <span>
+                        Per Project: $
+                        {new Intl.NumberFormat().format(job.budget)}
+                        .00 budget
+                      </span>
+                    )}
+                    {job.pay_type === 'Hourly' && (
+                      <span>
+                        Hourly: from $
+                        {new Intl.NumberFormat().format(job.min_pay)}/hr to $
+                        {new Intl.NumberFormat().format(job.max_pay)}/hr.
+                      </span>
+                    )}
+                  </div>
+                </div>
+                {/* Sanitize the description */}
+                <div className="job-list-description">
+                  {job.description
+                    ? DOMPurify.sanitize(
+                        job.description
+                          .replace(/<\/?p>/g, '')
+                          .replace(/<\/?br>/g, ' ')
+                          .replace(/<\/?strong>/g, '')
+                          .replace(/<\/?em>/g, '')
+                          .replace(/<\/?ul>/g, '')
+                          .replace(/<\/?ol>/g, '')
+                          .replace(/<\/?li>/g, '')
+                          .replace(/<\/?span>/g, '')
+                          .replace(/<\/?i>/g, '')
+                          .replace(/<\/?a>/g, '')
+                      ).substring(0, 250)
+                    : ''}
+                  ...
+                </div>
+                <div className="job-list-skills">
+                  {job.skills &&
+                    job.skills.split(',').map((skill, index) => (
+                      <span key={index} className="job-list-skill">
+                        {skill.trim()}
+                      </span>
+                    ))}
                 </div>
               </div>
-              {/* Sanitize the description */}
-              <div className="job-list-description">
-                {job.description
-                  ? DOMPurify.sanitize(
-                      job.description
-                        .replace(/<\/?p>/g, '')
-                        .replace(/<\/?br>/g, ' ')
-                        .replace(/<\/?strong>/g, '')
-                        .replace(/<\/?em>/g, '')
-                        .replace(/<\/?ul>/g, '')
-                        .replace(/<\/?ol>/g, '')
-                        .replace(/<\/?li>/g, '')
-                        .replace(/<\/?span>/g, '')
-                        .replace(/<\/?i>/g, '')
-                        .replace(/<\/?a>/g, '')
-                    ).substring(0, 250)
-                  : ''}
-                ...
-              </div>
-              <div className="job-list-skills">
-                {job.skills &&
-                  job.skills.split(',').map((skill, index) => (
-                    <span key={index} className="job-list-skill">
-                      {skill.trim()}
-                    </span>
-                  ))}
-              </div>
-            </div>
-          </Col>
-        </Row>
-      ))}
-      <div className="text-center mb-5">
-        <button
-          className="fl-button-primary"
-          onClick={prevPage}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-        <button onClick={nextPage} className="fl-button-primary">
-          Next
-        </button>
-      </div>
+            </Col>
+          </Row>
+        ))
+      )}
+      {jobs.length > 0 && (
+        <div className="text-center mb-5">
+          <button
+            className="fl-button-primary"
+            onClick={prevPage}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <button onClick={nextPage} className="fl-button-primary">
+            Next
+          </button>
+        </div>
+      )}
     </>
   );
 };
